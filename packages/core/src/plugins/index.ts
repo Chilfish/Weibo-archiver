5486bd495f001a879eb8a765c8c5a060ff08915f
Chilfish <chill4fish@gmail.com>
2023-08-25 18:15:22 +0800

    fix: build

---
+++ packages/core/src/plugins/index.ts
@@ -0,0 +1,4 @@
+import path from 'node:path'
+
+export const src = path.resolve(__dirname, '..')
+export const previewDir = path.resolve(src, '../../preview')
--- packages/core/src/plugins/initData.ts
+++ packages/core/src/plugins/initData.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
+import { previewDir, src } from '.'
 
 /**
  * init data before build
@@ -8,8 +9,15 @@ export default function InitData() {
   return {
     name: 'initData',
     async buildStart() {
-      const file = path.resolve(__dirname, '../stores/data.js')
-      fs.writeFile(file, 'export const _ = []\n')
+      const file = path.resolve(src, 'stores/data.js')
+      await fs.writeFile(file, 'export const _ = []\n')
+      await fs.rm(
+        path.resolve(previewDir, 'public/assets'),
+        {
+          recursive: true,
+          force: true,
+        },
+      )
     },
   }
 }
