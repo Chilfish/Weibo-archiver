d53e7416c3064c37fc510b9cd18c1883d698115a
Chilfish <chill4fish@gmail.com>
2024-03-03 21:25:31 +0800

    fixs

--- packages/core/src/utils/image.ts
+++ packages/core/src/utils/image.ts
@@ -14,9 +14,14 @@ export function replaceImg(img: string) {
 
   const imgHost = storage<string>('imgHost', '/')
 
+  // 使用 ipfs cdn
   if (imgHost === imgCdn) {
-    const { pathname } = new URL(img)
-    return `${imgCdn}${pathname}`
+    if (img.includes('sinaimg.cn')) {
+      const { pathname } = new URL(img)
+      return `${imgCdn}${pathname}`
+    }
+    // 像是 b 站、网易云这种外链的图片，就保持原样
+    return img
   }
 
   const name = img.split('/').pop()?.replace(/\?.+/, '') // 同时去除 params
@@ -26,9 +31,11 @@ export function replaceImg(img: string) {
     return img
   const fileName = `${prefix}-${name}`
 
+  // 本地图片
   if (imgHost === '/')
     return `http://localhost:3000/${fileName}`
 
+  // 自建图床
   const fixedBase = imgHost.endsWith('/') ? imgHost : `${imgHost}/`
   return `${fixedBase}${fileName}`
 }
--- packages/ui/src/post/Retweeted.vue
+++ packages/ui/src/post/Retweeted.vue
@@ -8,26 +8,28 @@ defineProps<{
 </script>
 
 <template>
-  <article class="mt-4 flex flex-col gap-2 rounded-2 bg-light p-3 dark:bg-dark">
+  <article
+    class="mt-4 flex flex-col gap-2 rounded-2 bg-light p-3 dark:bg-dark"
+  >
     <profile
-      v-if="post.user"
+      v-if="post.user?.id"
       :user="post.user"
     />
     <main>
       <post-text
         :text="post.text"
-        :class="post.user ? '' : 'text-red-7!'"
+        :class="post.user?.id ? '' : 'text-red!'"
       />
 
       <gallery :imgs="post.imgs" />
 
       <post-card
-        v-if="card"
+        v-if="card?.link"
         :card="card"
       />
     </main>
     <div
-      v-if="post.user"
+      v-if="post.user?.id"
       class="flex justify-between text-gray"
     >
       <post-meta :meta="post" />
