87d7eacba6f7f5ffccc8ea2f57cf8cc25f237bfb
Chilfish <chill4fish@gmail.com>
2024-05-12 23:32:13 +0800

    fix(web): css use `@apply` instead of `--uno`

--- packages/ui/src/post/Item.vue
+++ packages/ui/src/post/Item.vue
@@ -21,7 +21,7 @@ watchEffect(() => {
   <article
     :id="post.mblogid"
     ref="articleRef"
-    class="post"
+    class="post flex flex-col gap-2 rounded-2 bg-white p-4 shadow dark:bg-dark"
   >
     <div class="flex justify-between">
       <post-profile />
@@ -70,10 +70,6 @@ watchEffect(() => {
 </template>
 
 <style>
-article.post {
-  --uno: flex flex-col gap-2 rounded-2 bg-white p-4 shadow dark:bg-dark;
-}
-
 .post:hover .copy-id {
  opacity: 1 !important;
 }
