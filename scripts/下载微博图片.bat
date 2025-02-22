ce3eb5803d5d27055a30477c2abdf268bb1d2216
Chilfish <chill4fish@gmail.com>
2025-02-23 01:46:39 +0800

    chore: remove scripts

--- scripts/下载微博图片.bat
+++
@@ -1,7 +0,0 @@
-chcp 65001
-@echo off
-
-set /p imgsPath=请输入 imgs.csv 文件的完整路径（留空则使用默认路径） 
-
-node download.mjs -i %imgsPath%
-pause
--- scripts/运行图像服务器.bat
+++
@@ -1,6 +0,0 @@
-chcp 65001
-@echo off
-
-echo Weibo-archiver v0.4.4
-node server.mjs
-pause
