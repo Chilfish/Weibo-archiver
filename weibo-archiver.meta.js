07bd031deffb467dab6895136c6a87577200d0ed
Chilfish <chill4fish@gmail.com>
2025-03-07 23:26:04 +0800

    update

--- weibo-archiver.meta.js
+++ weibo-archiver.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Weibo Archiver
 // @namespace    chilfish/monkey
-// @version      0.4.4
+// @version      0.5.1
 // @author       Chilfish
 // @description  将你的新浪微博存档备份的油猴脚本，为号被完全夹没前绸缪 😭
 // @license      MIT
@@ -12,11 +12,10 @@
 // @downloadURL  https://github.com/Chilfish/Weibo-archiver/raw/monkey/weibo-archiver.user.js
 // @updateURL    https://github.com/Chilfish/Weibo-archiver/raw/monkey/weibo-archiver.meta.js
 // @match        https://weibo.com/u/*
-// @require      https://unpkg.com/vue@3.4.38/dist/vue.global.prod.js
-// @require      data:application/javascript,%3Bwindow.Vue%3DVue%3B
-// @require      https://unpkg.com/vue-demi@latest/lib/index.iife.js
-// @require      https://unpkg.com/pinia@2.2.2/dist/pinia.iife.js
-// @require      https://unpkg.com/naive-ui@2.39.0/dist/index.prod.js
+// @require      https://unpkg.com/vue@3.5.13/dist/vue.global.prod.js
 // @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @require      https://unpkg.com/axios@latest/dist/axios.min.js
+// @require      https://unpkg.com/dayjs@latest/dayjs.min.js
+// @require      https://unpkg.com/fuse.js@latest/dist/fuse.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
