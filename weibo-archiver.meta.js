a7a4d31f45f839b1e0b5433432fe09e3f8f28a42
Chilfish <chill4fish@gmail.com>
2025-06-27 17:52:19 +0800

    update

--- weibo-archiver.meta.js
+++ weibo-archiver.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Weibo Archiver
 // @namespace    chilfish/monkey
-// @version      0.6.0
+// @version      0.6.1
 // @author       Chilfish
 // @description  将你的新浪微博存档备份的油猴脚本，为号被完全夹没前绸缪 😭
 // @license      MIT
@@ -9,8 +9,8 @@
 // @homepage     https://github.com/Chilfish/Weibo-archiver
 // @source       https://github.com/Chilfish/Weibo-archiver/tree/main/packages/monkey
 // @supportURL   https://github.com/Chilfish/Weibo-archiver/issues
-// @downloadURL  https://github.com/Chilfish/Weibo-archiver/raw/monkey/weibo-archiver.user.js
-// @updateURL    https://github.com/Chilfish/Weibo-archiver/raw/monkey/weibo-archiver.meta.js
+// @downloadURL  https://github.com/Chilfish/Weibo-archiver/releases/latest/download/weibo-archiver.user.js
+// @updateURL    https://github.com/Chilfish/Weibo-archiver/releases/latest/download/weibo-archiver.meta.js
 // @match        https://weibo.com/*
 // @require      https://unpkg.com/vue@3.5.14/dist/vue.global.prod.js
 // @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
@@ -18,4 +18,3 @@
 // @require      https://unpkg.com/dexie@4.0.11/dist/dexie.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
-
